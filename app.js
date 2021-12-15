17df18943c1ad0b64a1e8d3bb0fe3a94e0c01de2
Luis Medeiros <luismedeiros@MacBook.local>
2021-12-15 09:08:08 -0300

    .

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const port = 3002;
+
+app.get('/.well-known/acme-challenge/SUehxNttlfuAZHEnZYD6JC25xrN2t4igvodgo75BzM0', (req, res) => {
+    res.send('SUehxNttlfuAZHEnZYD6JC25xrN2t4igvodgo75BzM0.y9h3wYHNKk_wb-5Zpcg_L5LpVReqOw3OyIUxeFyKmA0')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
